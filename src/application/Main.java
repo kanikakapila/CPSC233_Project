015849964def4b9b206fd241171da7494057590a
abrar.safwan <abrar.safwan@IC-CS233-21.uc.ucalgary.ca>
2022-07-22 19:53:54 -0600

    Added Title for the project

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Your Dream Car");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
