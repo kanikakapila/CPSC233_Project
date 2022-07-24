d23c804ba974b8f064e4f7ab3af7e91c80f4d1fb
abrar.safwan <abrar.safwan@IC-CS233-30.uc.ucalgary.ca>
2022-07-23 19:56:54 -0600

    Added Engine Size Slider,Labels and Fuel Method Choice Box

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,22 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = loader.load(new FileInputStream("C:\\Users\\abrar.safwan\\git\\CPSC233_Project\\src\\application\\CarPerformance.fxml"));
+			Scene scene = new Scene(root,600,400);
 			// Removed
 			primaryStage.setTitle("Your Dream Car");
 			primaryStage.setScene(scene);
