94c125d93c6370f4d37d790bd37c8c437421c131
kanika.kapila1 <kanika.kapila1@IC-CS233-12.uc.ucalgary.ca>
2022-07-24 20:08:17 -0600

    Added items to the Method of Fuel ChoiceBox

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			VBox root = loader.load(new FileInputStream("C:\\Users\\abrar.safwan\\git\\CPSC233_Project\\src\\application\\CarPerformance.fxml"));
+			VBox root = loader.load(new FileInputStream("C:\\Users\\kanika.kapila1\\git\\CPSC233_Project\\src\\application\\CarPerformance.fxml"));
 			Scene scene = new Scene(root,850,400);
 			// Removed
 			primaryStage.setTitle("Your Dream Car");
