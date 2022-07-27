ebc2cd48d03944aa0685f2040bad0f91351a49ef
abrar.safwan <abrar.safwan@IC-CS233-18.uc.ucalgary.ca>
2022-07-27 16:13:59 -0600

    Fixed the Master

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,32 +1,72 @@
 package application;
-	
+
+          
+
 import java.io.FileInputStream;
 
+ 
+
 import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
+
 import javafx.stage.Stage;
+
 import javafx.scene.Scene;
+
 import javafx.scene.layout.VBox;
 
+ 
 
+ 
 
-public class Main extends Application { 
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			FXMLLoader loader = new FXMLLoader(); 
-			VBox root = loader.load(new FileInputStream("src\\application\\CarPerformance.fxml"));
-			Scene scene = new Scene(root,900,400);
-			
-			primaryStage.setTitle("Your Dream Car");
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+ 
+
+public class Main extends Application {
+
+           @Override
+
+           public void start(Stage primaryStage) {
+
+                      try {
+
+                                 FXMLLoader loader = new FXMLLoader();
+
+                                 VBox root = loader.load(new FileInputStream("src\\application\\CarPerformance.fxml"));
+
+                                 //CarPerformanceController controller = (CarPerformanceController)loader.getController();
+
+                                 //controller.applicationStage = primaryStage;
+
+                                
+
+                                
+
+                                 Scene scene = new Scene(root,900,400);
+
+                                 // Removed
+
+                                 primaryStage.setTitle("Your Dream Car");
+
+                                 primaryStage.setScene(scene);
+
+                                 primaryStage.show();
+
+                      } catch(Exception e) {
+
+                                 e.printStackTrace();
+
+                      }
+
+           }
+
+          
+
+           public static void main(String[] args) {
+
+                      launch(args);
+
+           }
+
 }
+
